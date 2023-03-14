24303c671a54e27338e0ecef40b571deb84fa4ca
Sameer <sameerwaseem01@gmail.com>
2023-03-14 17:54:12 +0500

    path fix

--- next.config.js
+++ next.config.js
@@ -60,8 +60,8 @@ let basePath = '/';
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
+  assetPrefix = `/`;
+  basePath = `/`;
 }
 
 /**
