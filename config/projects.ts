044b75c3f691e784d662d1013448eb49ff56d723
Sameer <sameerwaseem01@gmail.com>
2023-03-15 14:42:17 +0500

    added autodeals to portfolio

--- config/projects.ts
+++ config/projects.ts
@@ -128,6 +128,24 @@ export const projects: Project[] = [
     subProjects: [],
   },
   {
+    title: 'AutoDeals',
+    slug: 'autodeals',
+    banner: '/static/projects/autodeals/banner.jpeg',
+    website: 'https://autodeals.pk',
+    description:
+      'Autodeals.pk is a website that serves as a platform for car dealers and individual sellers to list their vehicles for sale, as well as for buyers to search for and purchase cars in Pakistan. The website offers a variety of features to help buyers and sellers make informed decisions, such as browsing listings by make, model, price, and other criteria, viewing photos and specifications of each car, and contacting the seller directly through the website to ask questions. Autodeals.pk also offers a variety of resources and tools for car buyers and sellers in Pakistan, including articles on car buying and selling and a car loan calculator. Overall, Autodeals.pk is a convenient and useful resource for anyone looking to buy or sell a car in Pakistan.',
+    shortDescription:
+      'Autodeals.pk is a website for car dealers and sellers to list and buyers to purchase cars in Pakistan, offering features such as browsing, contacting sellers, and resources for buyers and sellers.',
+    repository: '',
+    stack: [Stack.javascript, Stack.php, Stack.mysql, Stack.react, Stack.node],
+    dimensions: [360, 640],
+    screenshots: ['/static/projects/autodeals/banner.jpeg'],
+    deployment: {
+      web: 'https://autodeals.pk',
+    },
+    subProjects: [],
+  },
+  {
     title: 'inPKR',
     slug: 'inpkr',
     banner: '/static/projects/inpkr/banner.png',
