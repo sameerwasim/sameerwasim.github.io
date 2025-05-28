ce9253ed5bc4b0cfef31fd99d03bc38d30a41dca
Sameer <sameerwaseem01@gmail.com>
2025-05-28 15:50:41 +0500

    Add Privacy Policy page for Gratuity Guru

---
+++ pages/gratuity-guru/privacy-policy.tsx
@@ -0,0 +1,39 @@
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <h3>Gratuity Guru</h3>
+      <p>Effective Date: May 28, 2025</p>
+
+      <p>
+        Gratuity Guru respects your privacy. This app does not collect, store,
+        or share any personal or non-personal data from its users.
+      </p>
+
+      <h5>Data Collection</h5>
+      <p>
+        Gratuity Guru does not collect any user data. All data created or used
+        within the app remains solely on the user's device and is deleted
+        automatically when the app is uninstalled.
+      </p>
+
+      <h5>Third-Party Services</h5>
+      <p>
+        This app does not use any third-party services that collect or process
+        user data.
+      </p>
+
+      <h5>Contact</h5>
+      <p>
+        If you have any questions about this Privacy Policy, you can contact us
+        at:
+      </p>
+      <p>
+        📧{' '}
+        <a href='mailto:sameerwaseem01@gmail.com'>sameerwaseem01@gmail.com</a>
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
