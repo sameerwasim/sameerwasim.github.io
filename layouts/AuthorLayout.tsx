ed5fb4687e98fe5b84e864f95f9d180608fceee5
Sameer <sameerwaseem01@gmail.com>
2026-01-13 20:13:25 -0500

    Add pnpm lockfile and update AuthorLayout image dimensions

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -28,8 +28,8 @@ export default function AuthorLayout({ children, frontMatter }: Props) {
               src={avatar}
               alt='avatar'
               width='192px'
-              height='192px'
-              className='h-48 w-48 rounded-full'
+              height='280px'
+              className='h-auto w-48 rounded-full'
             />
             <h3 className='pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight'>
               {name}
